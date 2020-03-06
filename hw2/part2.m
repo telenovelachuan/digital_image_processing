e023a08e80e58438af1e9cc41de8612caf3f235e
Chuan Sun <chuansun.sc@gmail.com>
2020-03-06 10:57:35 -0600

    pad fill

--- hw2/part2.m
+++ hw2/part2.m
@@ -1,11 +1,11 @@
 image = imread('alaska.jpg');
-h1=figure;imshow(image); title('original image');
-
-T = maketform('affine', [1 0 0; 0.3 1 0; 0 0 1] );
-color = [5 127 56]';
-R = makeresampler({'cubic','nearest'}, 'fill');
-B = imtransform(image, T, R, 'FillValues', color);
-h2=figure;imshow(B); title('sheared image');
+% h1=figure;imshow(image); title('original image');
+% 
+% T = maketform('affine', [1 0 0; 0.3 1 0; 0 0 1] );
+% color = [5 127 56]';
+% R = makeresampler({'cubic','nearest'}, 'fill');
+% B = imtransform(image, T, R, 'FillValues', color);
+% h2=figure;imshow(B); title('sheared image');
 
 % figure; subplot(2,2,1),imshow(image); title('Original Image');
 % 
@@ -30,28 +30,35 @@ h2=figure;imshow(B); title('sheared image');
 % end
 
 
-[U,V] = meshgrid(0:20:800,0:20:400);
-gray = 0.65 * [1 1 1];
- 
-figure(h1);
-hold on;
-line(U, V, 'Color', gray);
-line(U', V', 'Color', gray);
+% [U,V] = meshgrid(0:20:800,0:20:400);
+% gray = 0.65 * [1 1 1];
+%  
+% figure(h1);
+% hold on;
+% line(U, V, 'Color', gray);
+% line(U', V', 'Color', gray);
+% 
+%  
+% figure(h2);
+% hold on;
+% line(X, Y, 'Color',gray);
+% line(X',Y','Color',gray);
 
- 
-figure(h2);
-hold on;
-line(X, Y, 'Color',gray);
-line(X',Y','Color',gray);
+% gray = 1 * [1 1 1];
+% for u = 0:100:800
+%     for v = 0:100:400
+%         theta = (0 : 32)' * (2 * pi / 32);
+%         uc = u + 20*cos(theta);
+%         vc = v + 20*sin(theta);
+%         [xc,yc] = tformfwd(T,uc,vc);
+%         figure(h1); line(uc,vc,'Color',gray);
+%         figure(h2); line(xc,yc,'Color',gray);
+%     end
+% end
 
-gray = 1 * [1 1 1];
-for u = 0:100:800
-    for v = 0:100:400
-        theta = (0 : 32)' * (2 * pi / 32);
-        uc = u + 20*cos(theta);
-        vc = v + 20*sin(theta);
-        [xc,yc] = tformfwd(T,uc,vc);
-        figure(h1); line(uc,vc,'Color',gray);
-        figure(h2); line(xc,yc,'Color',gray);
-    end
-end
+
+R = makeresampler({'cubic','nearest'},'fill');
+color = [5 127 56]';
+Bf = imtransform(image, T, R,'XData',[-49 1000],'YData',[-49 500], 'FillValues',color);
+figure, imshow(Bf);
+title('Pad Method = ''fill''');
