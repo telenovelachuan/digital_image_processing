8a44f549f783f01105d4ed86710dead3f2f3450b
Chuan Sun <chuansun.sc@gmail.com>
2020-03-06 10:52:33 -0600

    transformation exploration

--- hw2/part2.m
+++ hw2/part2.m
@@ -1,4 +1,12 @@
 image = imread('alaska.jpg');
+h1=figure;imshow(image); title('original image');
+
+T = maketform('affine', [1 0 0; 0.3 1 0; 0 0 1] );
+color = [5 127 56]';
+R = makeresampler({'cubic','nearest'}, 'fill');
+B = imtransform(image, T, R, 'FillValues', color);
+h2=figure;imshow(B); title('sheared image');
+
 % figure; subplot(2,2,1),imshow(image); title('Original Image');
 % 
 % i = 1;
@@ -21,15 +29,29 @@ image = imread('alaska.jpg');
 %     subplot(2,2,i),imshow(B); title(strcat('color=', mat2str(color)));
 % end
 
-T = maketform('affine', [1 0 0; 0.3 1 0; 0 0 1] );
-color = [5 127 56]';
-R = makeresampler({'cubic','nearest'}, 'fill');
-B = imtransform(image, T, R, 'FillValues', color);
-[U,V] = meshgrid(0:20:800,0:20:400);
-[X,Y] = tformfwd(T,U,V);
-gray = 0.65 * [1 1 1];
 
-figure; imshow(B); title("sheared image");
+[U,V] = meshgrid(0:20:800,0:20:400);
+gray = 0.65 * [1 1 1];
+ 
+figure(h1);
+hold on;
+line(U, V, 'Color', gray);
+line(U', V', 'Color', gray);
+
+ 
+figure(h2);
 hold on;
 line(X, Y, 'Color',gray);
 line(X',Y','Color',gray);
+
+gray = 1 * [1 1 1];
+for u = 0:100:800
+    for v = 0:100:400
+        theta = (0 : 32)' * (2 * pi / 32);
+        uc = u + 20*cos(theta);
+        vc = v + 20*sin(theta);
+        [xc,yc] = tformfwd(T,uc,vc);
+        figure(h1); line(uc,vc,'Color',gray);
+        figure(h2); line(xc,yc,'Color',gray);
+    end
+end
