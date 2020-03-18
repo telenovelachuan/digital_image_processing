0afec946d341858631226d75bdca95a605ae2994
Chuan Sun <chuansun.sc@gmail.com>
2020-03-18 17:29:18 -0500

    hw3 q5

--- hw3/canny.m
+++ hw3/canny.m
@@ -1,7 +1,7 @@
 I = imread('ku.jpeg');
 %imshow(I);
 grey = rgb2gray(I);
-BW = edge(grey,'canny', [0.2 0.5]);
+%BW = edge(grey,'canny', [0.2 0.5]);
 %imshow(BW);
 
 % i = 0;
@@ -48,6 +48,14 @@ lines = houghlines(BW,theta,rho,P, 'FillGap', 5, 'MinLength', 7);
 % plot(xy_long(2,1),xy_long(2,2),'x','LineWidth',2,'Color','red');
 % plot(xy_long(:,1),xy_long(:,2),'LineWidth',2,'Color','red');
 
-y = [1 1.5 1 2 2.5]';
-A = [1 1; 2 1; 3 1; 4 1; 5 1 ];
-c = inv(A'*A)*A'*y
+% y = [1 1.5 1 2 2.5]';
+% A = [1 1; 2 1; 3 1; 4 1; 5 1 ];
+% c = inv(A'*A)*A'*y
+
+lab=[0 1 0;1 -4 1; 0 1 0];
+rez=uint8(filter2(lab,grey,'same'));
+imshow(rez);
+title('laplacian');
+
+E2 = edge(grey,'prewitt');
+figure; imshow(E2);
