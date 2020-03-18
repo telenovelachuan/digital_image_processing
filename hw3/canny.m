fa2d318d4c4b322922a20f145843c9712533cf6d
Chuan Sun <chuansun.sc@gmail.com>
2020-03-18 09:13:16 -0500

    q3 hough transform

--- hw3/canny.m
+++ hw3/canny.m
@@ -11,27 +11,30 @@ BW = edge(grey,'canny', [0.2 0.5]);
 %     subplot(2,2,i),imshow(bw); title(strcat('thresholds=[', num2str(a), ', ', num2str(a+0.3), ']'));
 % end
 
-[H,theta,rho] = hough(BW);
-figure
-imshow(imadjust(rescale(H)), [], 'XData',theta, 'YData',rho, 'InitialMagnification','fit');
-xlabel('\theta (degrees)')
-ylabel('\rho')
-axis on
-axis normal 
-hold on
-colormap(gca,hot)
+% [H,theta,rho] = hough(BW);
+% figure
+% imshow(imadjust(rescale(H)), [], 'XData',theta, 'YData',rho, 'InitialMagnification','fit');
+% xlabel('\theta (degrees)')
+% ylabel('\rho')
+% axis on
+% axis normal 
+% hold on
+% colormap(gca,hot)
 
 P = houghpeaks(H,5,'threshold',ceil(0.3*max(H(:))));
 lines = houghlines(BW,theta,rho,P, 'FillGap', 5, 'MinLength', 7);
 figure, imshow(grey), hold on
 max_len = 0;
 for k = 1:length(lines)
-   xy = [lines(k).point1; lines(k).point2];
-   plot(xy(:,1),xy(:,2),'LineWidth',2,'Color','green');
+    
+        xy = [lines(k).point1; lines(k).point2];
+    if k == 1
+        plot(xy(:,1),xy(:,2),'LineWidth',2,'Color','green');
 
-   % Plot beginnings and ends of lines
-   plot(xy(1,1),xy(1,2),'x','LineWidth',2,'Color','yellow');
-   plot(xy(2,1),xy(2,2),'x','LineWidth',2,'Color','red');
+       % Plot beginnings and ends of lines
+       plot(xy(1,1),xy(1,2),'x','LineWidth',2,'Color','yellow');
+       plot(xy(2,1),xy(2,2),'x','LineWidth',2,'Color','red');
+    end
 
    % Determine the endpoints of the longest line segment
    len = norm(lines(k).point1 - lines(k).point2);
@@ -41,4 +44,6 @@ for k = 1:length(lines)
    end
 end
 % highlight the longest line segment
+plot(xy_long(1,1),xy_long(1,2),'x','LineWidth',2,'Color','yellow');
+plot(xy_long(2,1),xy_long(2,2),'x','LineWidth',2,'Color','red');
 plot(xy_long(:,1),xy_long(:,2),'LineWidth',2,'Color','red');
