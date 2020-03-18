ea504055588bf9182845ecda5eb4cd686b6b257e
Chuan Sun <chuansun.sc@gmail.com>
2020-03-18 09:57:43 -0500

    fit dots

--- hw3/canny.m
+++ hw3/canny.m
@@ -23,27 +23,30 @@ BW = edge(grey,'canny', [0.2 0.5]);
 
 P = houghpeaks(H,5,'threshold',ceil(0.3*max(H(:))));
 lines = houghlines(BW,theta,rho,P, 'FillGap', 5, 'MinLength', 7);
-figure, imshow(grey), hold on
-max_len = 0;
-for k = 1:length(lines)
-    
-        xy = [lines(k).point1; lines(k).point2];
-    if k == 1
-        plot(xy(:,1),xy(:,2),'LineWidth',2,'Color','green');
+% figure, imshow(grey), hold on
+% max_len = 0;
+% for k = 1:length(lines)
+%     
+%         xy = [lines(k).point1; lines(k).point2];
+%     if k == 1
+%         plot(xy(:,1),xy(:,2),'LineWidth',2,'Color','green');
+% 
+%        % Plot beginnings and ends of lines
+%        plot(xy(1,1),xy(1,2),'x','LineWidth',2,'Color','yellow');
+%        plot(xy(2,1),xy(2,2),'x','LineWidth',2,'Color','red');
+%     end
+% 
+%    % Determine the endpoints of the longest line segment
+%    len = norm(lines(k).point1 - lines(k).point2);
+%    if ( len > max_len)
+%       max_len = len;
+%       xy_long = xy;
+%    end
+% end
+% % highlight the longest line segment
+% plot(xy_long(1,1),xy_long(1,2),'x','LineWidth',2,'Color','yellow');
+% plot(xy_long(2,1),xy_long(2,2),'x','LineWidth',2,'Color','red');
+% plot(xy_long(:,1),xy_long(:,2),'LineWidth',2,'Color','red');
 
-       % Plot beginnings and ends of lines
-       plot(xy(1,1),xy(1,2),'x','LineWidth',2,'Color','yellow');
-       plot(xy(2,1),xy(2,2),'x','LineWidth',2,'Color','red');
-    end
-
-   % Determine the endpoints of the longest line segment
-   len = norm(lines(k).point1 - lines(k).point2);
-   if ( len > max_len)
-      max_len = len;
-      xy_long = xy;
-   end
-end
-% highlight the longest line segment
-plot(xy_long(1,1),xy_long(1,2),'x','LineWidth',2,'Color','yellow');
-plot(xy_long(2,1),xy_long(2,2),'x','LineWidth',2,'Color','red');
-plot(xy_long(:,1),xy_long(:,2),'LineWidth',2,'Color','red');
+y = [1 1.5 1 2 2.5]';
+A = [1 1; 2 1; 3 1; 4 1; 5 1 ];
